4a3112ae51f18a2f084e2136f8b9acafd915d263
sunkarasairam18 <sunkarasairam@icloud.com>
2022-04-21 22:42:45 +0530

    21 April 2022

--- routes/address.js
+++ routes/address.js
@@ -4,21 +4,42 @@ const { user } = require('../middleware/user');
 const { auth } = require("../middleware/auth");
 const { Address } = require('../models/address');
 
+router.get('/get',[auth,user],async (req,res)=>{
+    try{
+        
+        const address = await Address.findOne({user: req.user._id});
+        if(address){
+            return res.status(200).send(address.AddressList);
+        }else return res.status(404).send("Addresses Not Found");
+    }catch(err){
+        return res.status(500).send(err); //Internal Server error
+    }
+});
+
 router.post('/add',[auth, user],async (req,res)=>{
     try{
         const address = await Address.findOne({user: req.user._id});
         if(address){
             const { _id } = req.body.address;
-            const add_item = address.AddressList.find(c => c._id == _id);
             let condition,update;
-            if(add_item){
-                condition = {"user": req.user._id,"AddressList._id": _id};
-                update = {
-                    "$set":{
-                        "AddressList.$": {
-                            ...req.body.address                            
+            if(_id){
+                const add_item = address.AddressList.find(c => c._id == _id);
+                if(add_item){
+                    condition = {"user": req.user._id,"AddressList._id": _id};
+                    update = {
+                        "$set":{
+                            "AddressList.$": {
+                                ...req.body.address                            
+                            }
                         }
                     }
+                }else{
+                    condition = {"user": req.user._id};
+                    update = {
+                        "$push":{
+                            "AddressList": req.body.address  
+                        }
+                    };              
                 }
             }else{
                 condition = {"user": req.user._id};
@@ -26,13 +47,13 @@ router.post('/add',[auth, user],async (req,res)=>{
                     "$push":{
                         "AddressList": req.body.address  
                     }
-                };              
+                };   
             }
             Address.findOneAndUpdate(condition,update,{new: true},(error,_address)=>{
                 if(error) return res.status(400).json({error});
                 if(_address){
                     
-                    return res.status(201).json(_address);
+                    return res.status(201).json(_address.AddressList);
                 }
             });
 
@@ -47,7 +68,7 @@ router.post('/add',[auth, user],async (req,res)=>{
                     return res.status(500).send(err); //Internal server eroor
                 }
                 if(result){
-                    return res.status(201).send("Address Added"); //Created
+                    return res.status(201).send(result.AddressList); //Created
                 }
             });
         }
